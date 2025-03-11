97e504d8e98c60d2189c063e2e21a41885ef3136
chetannn-github <chetan.rajawat25@gmail.com>
2025-03-11 14:11:17 +0530

    setup - basic routes setup.

---
+++ Client/src/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{headerShown:false}}>
+        
+        <Stack.Screen name="index"/>
+        <Stack.Screen name="auth" />
+        
+        
+        
+    </Stack>
+  )
+}
+
+export default Layout
