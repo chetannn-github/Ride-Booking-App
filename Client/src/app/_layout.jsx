df211a5cfe745cd6b06ba7c07c044b6c89a0975d
chetannn-github <chetan.rajawat25@gmail.com>
2025-03-13 18:40:53 +0530

    feat: add layout component with WebSocket provider and gesture handler support

--- Client/src/app/_layout.jsx
+++ Client/src/app/_layout.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {  Stack } from 'expo-router'
 import {WSProvider} from "../service/WSProvider"
 import {gestureHandlerRootHOC} from "react-native-gesture-handler"
+import { StatusBar } from 'react-native'
 
 const Layout = () => {
   return (
     <WSProvider>
+      <StatusBar barStyle={'dark-content'}></StatusBar>
       <Stack screenOptions={{headerShown:false}}>
         <Stack.Screen name="index"/>
-        <Stack.Screen name="auth" />
+        <Stack.Screen name='rider'/>
+        <Stack.Screen name='customer'/>
+        
     
       </Stack>
     </WSProvider>
